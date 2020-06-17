603841938b94d2b3fc4453beda5daebae82ca400
132Georgy <66915572+132Georgy@users.noreply.github.com>
2020-06-18 00:24:31 +0300

    Fourth commit
    Fixed code issues

--- resources/views/carmodels.blade.php
+++ resources/views/carmodels.blade.php
@@ -7,7 +7,7 @@
     <div class="starter-template">
       @foreach($carmodels as $carmodel)
             <div class="panel">
-            <a href="{{ route('carmodel') }}/{{$carmodel->code}}">
+            <a href="{{$carmodel->code}}">
                 <img src="http://internet-shop.tmweb.ru/storage/categories/mobile.jpg">
                 <h2>{{$carmodel->name}}</h2>
             </a>
@@ -16,4 +16,5 @@
             </p>
         </div>
   @endforeach
+</div>
 @endsection
