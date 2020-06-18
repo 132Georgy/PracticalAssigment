56d0dd7a10f214695401764e3a5a775811ec1882
ZooMieZoo <47496103+ZooMieZoo@users.noreply.github.com>
2020-06-18 22:44:58 +0300

    fixed cart and add to cart

--- resources/views/cartplace.blade.php
+++ resources/views/cartplace.blade.php
@@ -4,17 +4,17 @@
 
 @section('content')
 <div class="starter-template">
-                            <h1>Подтвердите заказ:</h1>
+                            <h1>Confirm Order:</h1>
     <div class="container">
         <div class="row justify-content-center">
-            <p>Общая стоимость: <b>0 ₽.</b></p>
+            <p>Total Price: <b>0 Eur.</b></p>
             <form action="http://internet-shop.tmweb.ru/basket/place" method="POST">
                 <div>
                     <p>Укажите свои имя и номер телефона, чтобы наш менеджер мог с вами связаться:</p>
 
                     <div class="container">
                         <div class="form-group">
-                            <label for="name" class="control-label col-lg-offset-3 col-lg-2">Имя: </label>
+                            <label for="name" class="control-label col-lg-offset-3 col-lg-2">Name: </label>
                             <div class="col-lg-4">
                                 <input type="text" name="name" id="name" value="" class="form-control">
                             </div>
@@ -22,7 +22,7 @@
                         <br>
                         <br>
                         <div class="form-group">
-                            <label for="phone" class="control-label col-lg-offset-3 col-lg-2">Номер телефона: </label>
+                            <label for="phone" class="control-label col-lg-offset-3 col-lg-2">Phone number: </label>
                             <div class="col-lg-4">
                                 <input type="text" name="phone" id="phone" value="" class="form-control">
                             </div>
@@ -37,7 +37,7 @@
                             </div>
                                             </div>
                     <br>
-                    <input type="hidden" name="_token" value="IDQ7ZubDesbUFWGlOgqg0sun3o0pyc37AB0Yvwlv">                    <input type="submit" class="btn btn-success" value="Подтвердите заказ">
+                    @csrf                    <input type="submit" class="btn btn-success" value="Confirm Order">
                 </div>
             </form>
         </div>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,30 +5,30 @@
 @section('content')
 
     <div class="starter-template">
-                            <h1>Все товары</h1>
+                            <h1>All Cars</h1>
     <form method="GET" action="http://internet-shop.tmweb.ru">
         <div class="filters row">
             <div class="col-sm-6 col-md-3">
-                <label for="price_from">Цена от                    <input type="text" name="price_from" id="price_from" size="6" value="">
+                <label for="price_from">Price from                    <input type="text" name="price_from" id="price_from" size="6" value="">
                 </label>
-                <label for="price_to">до                    <input type="text" name="price_to" id="price_to" size="6"  value="">
+                <label for="price_to">To                    <input type="text" name="price_to" id="price_to" size="6"  value="">
                 </label>
             </div>
             <div class="col-sm-2 col-md-2">
                 <label for="hit">
-                    <input type="checkbox" name="hit" id="hit" > Хит                </label>
+                    <input type="checkbox" name="hit" id="hit" > Bestsellers                </label>
             </div>
             <div class="col-sm-2 col-md-2">
                 <label for="new">
-                    <input type="checkbox" name="new" id="new" > Новинка                </label>
+                    <input type="checkbox" name="new" id="new" > New Arrivals                </label>
             </div>
             <div class="col-sm-2 col-md-2">
                 <label for="recommend">
-                    <input type="checkbox" name="recommend" id="recommend" > Рекомендуем                </label>
+                    <input type="checkbox" name="recommend" id="recommend" > Recommend                </label>
             </div>
             <div class="col-sm-6 col-md-3">
-                <button type="submit" class="btn btn-primary">Фильтр</button>
-                <a href="http://internet-shop.tmweb.ru" class="btn btn-warning">Сброс</a>
+                <button type="submit" class="btn btn-primary">Filter</button>
+                <a href="http://internet-shop.tmweb.ru" class="btn btn-warning">Reset</a>
             </div>
         </div>
     </form>
