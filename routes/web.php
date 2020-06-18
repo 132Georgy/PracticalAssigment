8612b00c73adab6d57ff7c2ca652d452c28a03a2
ZooMieZoo <47496103+ZooMieZoo@users.noreply.github.com>
2020-06-18 15:36:30 +0300

    commit seventh

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,8 @@ Route::get('/', 'ControllerMain@main')->name('main');
 Route::get('/carmodels', 'ControllerMain@carmodels')->name('carmodels');
 Route::get('/{carmodel}', 'ControllerMain@carmodel')->name('carmodel');
 Route::get('/{carmodel}/{product?}', 'ControllerMain@product')->name('product');
-<<<<<<< HEAD
 Route::get('/cart', 'CartController@cart')->name('cart');
 Route::get('/cart/order', 'CartController@order')->name('order');
 Route::post('cart/add/{id}','CartController@cartAdd')->name('cart-add');
-=======
 Route::get('/cart', 'ControllerMain@cart')->name('cart');
 Route::get('/cart/order', 'ControllerMain@order')->name('order');
->>>>>>> e10387212fe84bbef7887049262c3ae1ba996e6c
